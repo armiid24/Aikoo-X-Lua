260fd321a0497d8902fac20731aceac17bb735e5
armiid24 <armikogg@gmail.com>
2025-08-22 21:24:48 +0700

    Update Loader.lua

--- Aikoo X Lua/Loader.lua
+++ Aikoo X Lua/Loader.lua
@@ -1,2 +1,2 @@
-local MainUIScript = game:HttpGet("https://raw.githubusercontent.com/username/repo/main/MainUIFrame.lua")
+local MainUIScript = game:HttpGet("https://raw.githubusercontent.com/armiid24/Aikoo-X-Lua/refs/heads/main/Aikoo%20X%20Lua/MainUIFrame.lua")
 loadstring(MainUIScript)()
