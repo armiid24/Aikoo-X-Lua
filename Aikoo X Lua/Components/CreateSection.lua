dd173cefe1abdb0806ab067521ef8e27c4a25581
armiid24 <armikogg@gmail.com>
2025-08-22 21:16:41 +0700

    Add files via upload

--- Aikoo X Lua/Components/CreateSection.lua
+++
@@ -1,8 +0,0 @@
--- CreateSection(title)
-return function(title)
-    local section = Instance.new("Frame")
-    section.Name = title .. "_Section"
-    section.Size = UDim2.new(1, 0, 0, 50)
-    section.BackgroundTransparency = 1
-    return section
-end
